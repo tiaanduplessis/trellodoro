e708ff638bea1b5b5d54fcc4608791eaf36660b0
Anthony Sousa <anthony.acsousa@gmail.com>
2016-03-25 19:41:42 -0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Trellodoro - A Simple Pomodoro Timer Extension for Trello
+
+## Work in progress
+- Animations
+- Custom options (Notification text, pomodoro time)
+- Option to add "rest" time
+
+## How to use
+Access [chrome://extensions], mark the "Developer Mode" checkbox and click on "Load unpacked extension.." the Trellodoro folder.
+
+[chrome://extensions]: <chrome://extensions>
---
+++ trellodoro.js
@@ -0,0 +1,69 @@
+
+var trellodoroLogo =  (window.location.href.indexOf("trello.com") > -1) ? chrome.extension.getURL('trellodoro-logo.png') : 'trellodoro-logo.png';
+var isPaused = true;
+var timerId;
+var trellodoroSettings = {
+  time:25, // Pomodoro time
+  title:"Próximo!", // Notification title
+  message:"Você avassalou em mais um trellodoro! Pronto pro outro?", // Notification message
+}
+
+// Request permission to use Chrome Notification Messages
+document.addEventListener('DOMContentLoaded', function () {
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+});
+
+function trellodoroNotify() {
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+  else {
+    var trellodoroAlert = new Notification(trellodoroSettings.title, {
+      icon: trellodoroLogo,
+      body: trellodoroSettings.message,
+    });
+  }
+}
+
+function trellodoro(timer) {
+  var countdown = timer * 60 * 1000;
+  clearInterval(timerId);
+  $("#trellodoro-counter").hide().fadeIn('fast');
+  timerId = setInterval(function(){
+    if (!isPaused) {
+      countdown -= 1000;
+      var min = Math.floor(countdown / (60 * 1000));
+      var sec = Math.floor((countdown - (min * 60 * 1000)) / 1000);
+
+      if (countdown <= 0) {
+        trellodoroNotify();
+        clearInterval(timerId);
+        $("#trellodoro-counter").html(" 00:00 ");
+      } else {
+         $("#trellodoro-counter").html(min + ":" + sec);
+      }
+    }
+  }, 1000);
+}
+
+$(function() {
+  $('#header .header-user').before('<div id="trellodoro-container"><div id="trellodoro-start"><img src="'+ trellodoroLogo +'" alt="Trellodoro"></div><div id="trellodoro-counter">00:00</div><div id="trellodoro-play">&#9654;</div><div id="trellodoro-pause">&#10074;&#10074;</div></div>');
+
+  $('#trellodoro-start').click(function() {
+    isPaused = false;
+    trellodoro(trellodoroSettings.time);
+    $('#trellodoro-container').addClass('trellodoroStarted');
+  });
+
+  $('#trellodoro-play').click(function() {
+    isPaused = false;
+    $(this).hide();
+    $('#trellodoro-pause').show();
+  });
+
+  $('#trellodoro-pause').click(function() {
+    isPaused = true;
+    $(this).hide();
+    $('#trellodoro-play').show();
+  });
+});
